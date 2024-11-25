836760d33096b377e41bd0d6f1cee63150711369
CodeMunDee <codemundee@gmail.com>
2024-11-25 19:49:25 +0700

    update code

--- src/lib/Component.svelte
+++ src/lib/Component.svelte
@@ -5,23 +5,26 @@
 
 	let {
 		children,
-		as = 'span',
+		as = "span",
 		duration = 500,
 		class: className,
 		rippleClass = "bg-white/25",
 		...restProps
 	}: Props = $props();
+
+	const rootClasses = "pointer-events-none absolute bottom-0 left-0 right-0 top-0 overflow-hidden";
+	const rippleClasses = "pointer-events-none absolute rounded-full";
+
+	let rootCls = $derived.by(() => twMerge(rootClasses, className));
+	let rippleCls = $derived.by(() => twMerge(rippleClasses, rippleClass));
 </script>
 
 <svelte:element
 	this={as}
-	class={twMerge(
-		"pointer-events-none absolute bottom-0 left-0 right-0 top-0 overflow-hidden",
-		className,
-	)}
+	class={rootCls}
 	use:ripple={{
 		duration,
-		classes: twMerge("pointer-events-none absolute rounded-full", rippleClass),
+		classes: rippleCls,
 	}}
 	{...restProps}
 >
